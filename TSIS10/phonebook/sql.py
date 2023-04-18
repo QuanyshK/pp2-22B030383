b4e501a403c55af258f0fc293eabdebd3aba4e87
QuanyshK <k_kobeisinuly@kbtu.kz>
2023-04-19 02:15:59 +0600

    first task lab 10

--- TSIS10/phonebook/sql.py
+++ TSIS10/phonebook/sql.py
@@ -1,91 +1,56 @@
+import psycopg2
 import csv
-import psycopg2 
+con = psycopg2.connect(host = "localhost", database = "phonebook", user = "postgres", password = "kukamerey26")
+current = con.cursor()
 
+insert = """
+    INSERT INTO PhoneBook VALUES(%s,%s,%s) returning *;
+"""
 
-connection=psycopg2.connect(host="localhost", dbname="postgres", user="postgres", 
-                         password="123456")
-cur=connection.cursor()
+update = """
+    UPDATE PhoneBook SET number = %s WHERE name = %s;
+"""
 
-cur.execute("""CREATE TABLE IF NOT EXISTS PhoneBook (
-    surname VARCHAR(255),
-    name VARCHAR(255),
-    number INT
-);
-""")
+select = """
+    SELECT * FROM PhoneBook
+"""
 
-def update(sn, mode, newv):
-    cur.execute("""UPDATE PhoneBook
-    SET {} = '{}'
-    WHERE surname = '{}'
-    """.format(mode,newv,sn))
-
-def delete(sn):
-    cur.execute("""DELETE FROM Phonebook
-    WHERE surname='{}'
-    """.format(sn))
-
-#INSERTING DATA--------------------------
-
-mode="enter";
-while True:
-    print("Type 'enter' if you want to add more data and type 'stop' to break")
-    mode=input()
-    if mode=="stop":
-        break
-    mytuple=[]
-    print("enter surname:")
-    mytuple.append(input())
-    print("enter name:")
-    mytuple.append(input())
-    print("enter number:")
-    mytuple.append(input())
-    mytuple=tuple(mytuple)
-    cur.execute("""INSERT INTO PhoneBook (surname, name , number) VALUES
-    {};
-    """.format(mytuple))
+delete = """
+    DELETE FROM PhoneBook WHERE name = %s;
+"""
 
 while True:
-    print("Want to insert data from csv file? yes/no:")
-    mode=input()
-    if mode=="no":
+    n = int(input(" 1 - insert csv, 2 - insert console, 3 - update, 4 - select, 5 - delete, 6 - exit\n"))
+    if n == 1:
+        with open("p4.csv", "r") as f:
+                reader = csv.reader(f, delimiter=",")
+                for row in reader:
+                    current.execute(insert, row)
+        con.commit()
+    if n == 2:
+            name = input("Name:")
+            surname = input("Sname:")
+            phoneNumber = input("Number:")
+            current.execute(insert, (name, surname, phoneNumber))
+            con.commit()
+            
+    if n == 3:
+        name = input("Name:")
+        phone_number = input("newnumber:")
+        current.execute(update, (phone_number,name))
+        con.commit()
+    if n == 4:
+        current.execute(select)
+        print(*current.fetchall(), sep='\n')
+        con.commit()
+    if n == 5:
+        name = input("Name:")
+        current.execute(delete, [name])
+        con.commit()
+    if n == 6:
         break
-    print("enter the name of the file")
-    mode=input()
-    with open(mode + '.csv', 'r') as f:
-        reader = csv.reader(f)
-        next(reader)
-        for row in reader:
-            cur.execute("INSERT INTO PhoneBook VALUES (%s,%s,%s)",row)
-
-#UPDATING DATA---------
-while True:
-    print("Type 'update' to update some data or 'stop' to break")
-    mode=input()
-    if mode=="stop":
-        break
-    cur.execute("""SELECT * FROM PhoneBook""")
-    print(cur.fetchall())
-    print("Enter surname")
-    idtochange=input()
-    print("What you want to change? name/number")
-    mode=input()
-    print("Enter new name/number")
-    newvalue=input()
-    update(idtochange, mode, newvalue)
-
-#DELETING DATA-----------
-while True:
-    print("want to delete some data? yes/no")
-    mode=input()
-    if mode=="no":
-        break
-    cur.execute("""SELECT * FROM PhoneBook""")
-    print(cur.fetchall())
-    print("Enter surname")
-    idtodelete=input()
-    delete(idtodelete)
 
 
-connection.commit()
-cur.close()
-connection.close()
+current.close()
+con.commit()
+con.close()
