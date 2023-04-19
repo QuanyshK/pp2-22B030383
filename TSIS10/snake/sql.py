6cc5b5626a0ce3ec73b2c4db59213c34880b6419
QuanyshK <k_kobeisinuly@kbtu.kz>
2023-04-20 01:02:40 +0600

    update for snake lab 10

--- TSIS10/snake/sql.py
+++ TSIS10/snake/sql.py
@@ -1,168 +1,189 @@
-import csv
-import psycopg2
+
 import pygame
-import random
 from random import randrange
+import time
+import psycopg2
 
-conn = psycopg2.connect(host = "localhost", user = "postgres", database = "snake", password = "kukamerey26")
-cur = conn.cursor()
-createtb = ('''
-    CREATE TABLE IF NOT EXISTS snake(
-    name varchar,
-    scores varchar
-    )''')
+size = width, height = 1050, 650
+block = 25
+
+x, y = randrange(0, width, block), randrange(0, height, block)
+apple = randrange(0, width, block), randrange(0, height, block)
+orange = randrange(0, width, block), randrange(0, height, block)
+dirs = {'W': True, 'S': True, 'A': True, 'D': True}
+length = 1
+score = 0
+level = 0
+snake = [(x, y)]
+dx, dy = 0, 0
+fps = 10
+walls = []
+
 pygame.init()
-level=1
-level1=1
-level3=20
-level2=9
-sup = 10
-mon=800
-size=50
-fps=60
-bg = pygame.image.load("background.jpg")
-bg=pygame.transform.scale(bg,(800,800)) 
-x,y= 300,300
-length=1 
-snake=[(x,y)]
+screen = pygame.display.set_mode([width, height])
+clock = pygame.time.Clock()
+font_score = pygame.font.SysFont('Arial', 26, bold=True)
+font_end = pygame.font.SysFont('Arial', 66, bold=True)
+font_level = pygame.font.SysFont('Arial', 26, bold=True)
+font_menu = pygame.font.SysFont('Arial', 85,bold=True)
+game_background = pygame.image.load('background.jpg').convert()
+menu_background = pygame.image.load('menu_background.jpg').convert()
 
 
-buttons={
-    'w':True,
-    'a':True,
-    'd':True,
-    's':True
-}
-score_font = pygame.font.SysFont("Verdana", 20)
-level_font = pygame.font.SysFont("Verdana", 20)
-front=pygame.font.SysFont('Ariel',26,True)
-def add_user():
-    name = input("Newuser:")
-    score = input("Score:")
-    cur.execute("INSERT INTO snake VALUES(%s, %s)", (name, score))
-    conn.commit()
-def show():
-    name = input("User:")
-    cur.execute("SELECT * FROM snake WHERE name ILIKE %s", (name, score))
-    conn.commit()
-def delete():
-    name = input("Delete user:")
-    cur.execute("DELETE FROM snake name ILIKE %s", [name])
-    conn.commit()
+def login(name):
+    cur.execute("SELECT name, score, level FROM snake")
+    row = cur.fetchall()
+    for rows in row:
+        if name == rows[0]:
+            print(f'You have successfully logged into your account {name}. \nYour data: Score: {rows[1]}\nLevel: {rows[2]}.')
+            return False
+    return True
 
-count=0
 
-walls={ #уровни препятствий,
-    1:[(50,50),(50,100),(50,150),(50,200),(150,0),(200,0),(250,0),(500,500),(550,500),(500,550),(600,500),(650,500)],
-    2:[(700,700),(650,700),(700,650),(600,700),(700,600),(700,550),(650,550),(650,500)],
-    3:[(0,0),(50,0),(100,0),(150,0),(200,0),(250,0),(300,0),(350,0),(400,0),(450,0),(500,0),(550,0),(600,0),(650,0),(700,0),(0,50),(0,100),(0,150),(0,200),(0,250),(0,300),(0,350),(0,400),(0,450),(0,500),(0,550),(0,600),(0,650),(0,700)]
-}
+def record(name):
+    cur.execute("SELECT name, score, level from snake")
+    row = cur.fetchall()
+    for rows in row:
+        if name == rows[0]:
+            if result[1] >= int(rows[2]) and result[0] > int(rows[1]):
+                cur.execute(f"UPDATE snake set score = {str(result[0])}, level = {str(result[1])} where name = '{name}'")
+                con.commit()
+                return True
+    return False
 
-speed=7 #скорость для clock
-sup=10 #для 44,45 а так же для 124 строки нужнo, для range чтобы узнать попадает ли score в range
-score=2# очки за каждое яблоко
-dx,dy=0,0 #направление 
 
-clock=pygame.time.Clock()
-time=50 
+global con, cur
 
-apple=randrange(50,mon,size),randrange(0,mon,size) #спавн яблока
-while apple in walls:
-    apple=randrange(50,mon,size),randrange(0,mon,size)
+con = psycopg2.connect(
+    database='snake',
+    user='postgres',
+    password='kukamerey26',
+    host="localhost",
+)
 
-screen=pygame.display.set_mode([mon,mon])
+cur = con.cursor()
+
+
+name = input('Input your name: ')
+
+if login(name):
+    cur.execute(f'''INSERT INTO snake (name, score, level) VALUES('{name}', 0, 0)''')
+    con.commit()
+    print('Welcome, new player!')
+    
+con.commit()
 
 
 
-working=True
-while working:
+def main_menu():
+    menu = True
+    while menu:
+        screen.blit(menu_background, (0, 0))
+        
+        # menu text
+        render_menu = font_menu.render('CLICK', True, pygame.Color('black'))
+        screen.blit(render_menu, (430, 550))
+
+        for event in pygame.event.get():
+            mx, my = pygame.mouse.get_pos()
+
+            if event.type == pygame.QUIT:
+                exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN and 430 <= mx <= 550 and 570 <= my <= 600:
+                menu = False
+
+        pygame.display.flip()
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_RETURN]: # K_RETURN - нажатие на Enter
+            break
+
+main_menu()
+
+while True:
+    screen.blit(game_background, (0, 0))
+
+    # drawing snake, apple, walls
+    for i, j in snake:
+        pygame.draw.rect(screen, pygame.Color('white'), (i, j, block - 1, block - 1))
+    pygame.draw.rect(screen, pygame.Color('red'), (*apple, block, block))
+    if score % 3 == 0 and score > 0:
+        pygame.draw.rect(screen, pygame.Color('orange'), (*orange,block,block))
+    for i, j in walls:
+        pygame.draw.rect(screen, pygame.Color('black'), (i, j, block - 1, block - 1))
+
+    # show score
+    render_score = font_score.render(f'SCORE: {score}', True, pygame.Color('orange'))
+    screen.blit(render_score, (5, 5))    
+    render_level = font_level.render(f'LEVEL: {level}', True, pygame.Color('aqua'))
+    screen.blit(render_level, (880,5))
+    
+    # snake movement
+    x += dx * block
+    y += dy * block
+    snake.append((x, y))
+    snake = snake[-length:]
+    
+
+    # game over
+    if x < 0 or x > width - block or y < 0 or y > height - block\
+    or len(snake) > len(set(snake)) or snake[-1] in walls:
+        while True:
+            render_end = font_end.render('GAME OVER', True, pygame.Color('orange'))
+            screen.blit(render_end, (335, 300))
+            pygame.display.flip()
+            result = [score, level]
+            if record(name):
+                print(f'New record: {score}.')
+    # eating apple
+    if snake[-1] == apple:
+        while apple in snake or apple in walls:
+            apple = randrange(0, width, block), randrange(0, height, block)
+        length += 1
+        score += 1
+        fps += 0.5
+        if score % 3 == 0 and score > 0:
+            level += 1
+        if level > 3:
+            score += 3
+            length += 2
+        wall = randrange(0, width, block), randrange(0, width, block)
+        walls.append(wall)
+    if snake[-1] == orange:
+        while orange in snake or orange in walls:
+            orange = randrange(0, width, block), randrange(0, height, block)
+        length += 2
+        score += 2
+        fps += 1
+        if score % 3 == 0 and score > 0:
+            level += 1
+        if level > 3:
+            score += 3
+            length += 2
+        wall = randrange(0, width, block), randrange(0, width, block)
+        walls.append(wall)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
-            working=False
-
-        if event.type == pygame.KEYDOWN :# полностью движение 
-            if event.key == pygame.K_w and buttons['w']: # если нажали w то змейка будет двигать вверх, ибо dy будет равен -1(тоесть вы будете двигаться по координа dy в минусовыю часть, тоесть вверх)
-                #а buttonsp['w'] нужен для того чтобы при движений вниз s  мы не могли двигаться одновременно вверх
-                buttons={
-                    'w':True,
-                    'a':True,
-                    'd':True,
-                    's':False
-                }
-                dx,dy=0,-1
-            elif event.key == pygame.K_s and buttons['s']:
-                dx,dy=0,1
-                buttons={
-                    'w':False,
-                    'a':True,
-                    'd':True,
-                    's':True
-                }
-            elif event.key == pygame.K_a and buttons['a']:
-                dx,dy=-1,0
-                buttons={
-                    'w':True,
-                    'a':True,
-                    'd':False,
-                    's':True
-                }
-            elif event.key == pygame.K_d and buttons['d']:
-                dx,dy=1,0
-                buttons={
-                    'w':True,
-                    'a':False,
-                    'd':True,
-                    's':True #sw
-                }
-    screen.blit(bg,(0,0)) #рисуем задний фон
-
-    if snake[-1] in walls[level]: # если координаты головы есть в координатах стен то игра останавливается
-        pygame.quit()
-
-    if apple in walls[level]:#нужна для проверки если яблоко появилось внутри стен
-        apple=randrange(50,mon,size),randrange(0,mon,size)
-
-    [(pygame.draw.rect(screen,pygame.Color('green'),(i,j,size-2,size-2))) for i, j in snake]# рисуем змейку
-
-    pygame.draw.rect(screen, pygame.Color('red'), (*apple, size-2,size-2))# рисуем яблоко
-
-    for i in walls[level]: #рисуем стены определенного уровня
-        pygame.draw.rect(screen,(0,0,0),(i[0],i[1],48,48))
-    render_score=front.render(f'SCORE:{count}',1,pygame.Color('purple'))#пишем score
-    screen.blit(render_score,(5,5))#рисуем score
-    render_level=front.render(f'LEVEL:{level1}',1,pygame.Color('purple'))#пишем score
-    screen.blit(render_level,(5,20))
-    if time ==0:#время для случая, если время для сьедания яблока истекло
-        apple=randrange(50,mon,size),randrange(0,mon,size)
-        time=50
-
-    x+=dx*size #непрерывное движение
-    y+=dy*size
-
-    time-=1 #раз в 50 speed мы обновляем местоположение яблоки, это нужно нам для 111 строки
-
-    snake.append((x,y))#координаты змейки
-   
-    snake=snake[-length:]#для того чтобы мы брали только змейку, а не места где она прошлась
-
-    if count in range(level2,level3):#нужна для переноса в след уровень,а так же для увеличения награды за яблоко, скорость, 
-        level1+=1
-        level2+=sup
-        level3+=sup
-        sup+=20
-        level+=1
-        score+=1
-        speed+=2
-        if level==4:level=1
-
-    if snake[-1]==apple: #в случае если координата яблоки и головы змейки совпала
-        count+=score
-        length+=1
-        time=50
-        apple=randrange(50,mon,size),randrange(0,mon,size)
-
-    if x<0 or x> mon-size or y<0 or y >mon-size or len(snake) != len(set(snake)):#выход за пределы поля или змейка входит в себя
-        working=False
+            False
 
     pygame.display.flip()
-    clock.tick(speed)
+    clock.tick(fps)
+
+    # control
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w] and dirs['W']:
+        dx, dy = 0, -1
+        dirs = {'W': True, 'S': False, 'A': True, 'D': True}
+    if key[pygame.K_s] and dirs['S']:
+        dx, dy = 0, 1
+        dirs = {'W': False, 'S': True, 'A': True, 'D': True}
+    if key[pygame.K_a] and dirs['A']:
+        dx, dy = -1, 0
+        dirs = {'W': True, 'S': True, 'A': True, 'D': False}
+    if key[pygame.K_d] and dirs['D']:
+        dx, dy = 1, 0
+        dirs = {'W': True, 'S': True, 'A': False, 'D': True}
+con.commit()
+cur.close()
+con.close()
